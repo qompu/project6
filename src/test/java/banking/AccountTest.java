59b116ac6e41291f20c308670dd101e29c9819df
Adriel Lopez <alopez216@hawkmail.hccfl.edu>
2021-04-20 18:03:01 -0400

    test methods added

---
+++ src/test/java/banking/AccountTest.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Adriel
+ */
+public class AccountTest {
+    
+    public AccountTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+
+    /**
+     * Test of deposit method, of class Account.
+     */
+    @Test
+    public void testDeposit() {
+    }
+
+    /**
+     * Test of withdraw method, of class Account.
+     */
+    @Test
+    public void testWithdraw() {
+    }
+
+    /**
+     * Test of transfer method, of class Account.
+     */
+    @Test
+    public void testTransfer() {
+    }
+
+    /**
+     * Test of getAccountDescription method, of class Account.
+     */
+    @Test
+    public void testGetAccountDescription() {
+    }
+
+    /**
+     * Test of setAccountDescription method, of class Account.
+     */
+    @Test
+    public void testSetAccountDescription() {
+    }
+
+    /**
+     * Test of getBalance method, of class Account.
+     */
+    @Test
+    public void testGetBalance() {
+    }
+
+    /**
+     * Test of getTransactions method, of class Account.
+     */
+    @Test
+    public void testGetTransactions() {
+    }
+
+    /**
+     * Test of getTransaction method, of class Account.
+     */
+    @Test
+    public void testGetTransaction() {
+    }
+
+    /**
+     * Test of getAccountId method, of class Account.
+     */
+    @Test
+    public void testGetAccountId() {
+    }
+
+    /**
+     * Test of getCustomerId method, of class Account.
+     */
+    @Test
+    public void testGetCustomerId() {
+    }
+
+    /**
+     * Test of getAccountCreationDate method, of class Account.
+     */
+    @Test
+    public void testGetAccountCreationDate() {
+    }
+
+    /**
+     * Test of hashCode method, of class Account.
+     */
+    @Test
+    public void testHashCode() {
+    }
+
+    /**
+     * Test of equals method, of class Account.
+     */
+    @Test
+    public void testEquals() {
+    }
+
+    /**
+     * Test of compareTo method, of class Account.
+     */
+    @Test
+    public void testCompareTo() {
+    }
+
+    /**
+     * Test of toString method, of class Account.
+     */
+    @Test
+    public void testToString() {
+    }
+
+    public class AccountImpl extends Account {
+
+        public AccountImpl() {
+            super(null, 0.0, "");
+        }
+
+        public void deposit(double amount) {
+        }
+
+        public void withdraw(double amount) {
+        }
+    }
+    
+}
