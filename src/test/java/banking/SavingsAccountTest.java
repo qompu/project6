1fc3adeed0dff27040b4301ac7c01ccd5f53398a
Adriel Lopez <alopez216@hawkmail.hccfl.edu>
2021-04-22 21:07:15 -0400

    savings account update

---
+++ src/test/java/banking/SavingsAccountTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Adriel
+ */
+public class SavingsAccountTest {
+    
+    public SavingsAccountTest() {
+        
+        String customerID = "12345";
+        double initialBalance = 300.0;
+        String description = "John Smith's account"; 
+        double interestRate = 0.25;
+    }
+    
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    /**
+     * Test of deposit method, of class SavingsAccount.
+     */
+    @Test
+    public void testDeposit() {
+        double actual = 100.00;
+        double expected = 100.00;
+        
+        assertEquals(expected, actual);
+        
+        
+    }
+
+    /**
+     * Test of withdraw method, of class SavingsAccount.
+     */
+    @Test
+    public void testWithdraw() {
+        double actual = 100.00;
+        double expected = 100.00;
+        
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test of addInterestTransaction method, of class SavingsAccount.
+     */
+    @Test
+    public void testAddInterestTransaction() {
+        double actual = 100.00;
+        double expected = 100.00;
+        
+        assertEquals(expected, actual);
+        
+        
+        
+    }
+
+    /**
+     * Test of getDefaultInterestRate method, of class SavingsAccount.
+     */
+    @Test
+    public void testGetDefaultInterestRate() {
+        
+    }
+
+    /**
+     * Test of setDefaultInterestRate method, of class SavingsAccount.
+     */
+    @Test
+    public void testSetDefaultInterestRate() {
+    }
+    //
+}
