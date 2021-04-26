6e8975db7f8a950cc9caeaf86ea401ea7d0ddaf6
qompu <mtamayo11@hawkmail.hccfl.edu>
2021-04-25 22:15:54 -0400

    added comments

--- src/test/java/banking/SavingsAccountTest.java
+++ src/test/java/banking/SavingsAccountTest.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Group: Coffee123
+ * Project 6
+ * Class SavingsAccount.java tests the functionality of Bank.java
+ * 
  */
 package banking;
 
